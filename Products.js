3a1d1abf12c077332ea5445486aae9c7f8937b26
Gentility <marshamarther4@gmail.com>
2026-06-23 15:37:41 -0400

    Create Products.js
    const products = [
    
      {id:1,name:"Men's T-Shirt",price:8000,category:"clothes",image:"https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"},
    
      {id:2,name:"Fashion Sneakers",price:25000,category:"shoes",image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff"},
    
      {id:3,name:"Smart Watch",price:25000,category:"electronics",image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30"},
    
      {id:4,name:"Rice 5kg",price:5000,category:"groceries",image:"https://images.unsplash.com/photo-1603048297172-c92544798d5a"}
    
    ];

---
+++ Products.js
@@ -0,0 +1,11 @@
+const products = [
+
+  {id:1,name:"Men's T-Shirt",price:8000,category:"clothes",image:"https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"},
+
+  {id:2,name:"Fashion Sneakers",price:25000,category:"shoes",image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff"},
+
+  {id:3,name:"Smart Watch",price:25000,category:"electronics",image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30"},
+
+  {id:4,name:"Rice 5kg",price:5000,category:"groceries",image:"https://images.unsplash.com/photo-1603048297172-c92544798d5a"}
+
+];
